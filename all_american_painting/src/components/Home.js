485befd10958cad9a8624dc7c5e7b36948e59a0e
JacquesFr <jacques@koalescedesigns.com>
2020-12-02 14:25:36 -0800

    Added banner.js and navbar.js

--- all_american_painting/src/components/Home.js
+++ all_american_painting/src/components/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>All American Painting - Home </h1>
+            <h1>All American Painting - Home: slide show </h1>
+            <h1>All American Painting - Home: Why us?</h1>
         </div>
     );
 };
