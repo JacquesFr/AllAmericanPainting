a35703acc5e5365eadd4984877f4cabd2a467f60
JacquesFr <jacques@koalescedesigns.com>
2020-12-02 15:12:47 -0800

    Imported react-bootstrap

--- all_american_painting/src/components/Banner.js
+++ all_american_painting/src/components/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
 
 const Banner = () => {
     return (
         <div>
-            <h1>Header: Banner </h1>
+            <Jumbotron>
+                <h1>All American Painting</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+                </p>
+            </Jumbotron>
         </div>
     );
 };
