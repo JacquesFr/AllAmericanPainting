fa0a27eb12f3bf949221b16feea5e8382771c2f2
Ashley Taylor McDaniel <43732431+ASHTM15@users.noreply.github.com>
2020-12-03 17:29:42 -0800

    Working NavBar

--- all_american_painting/src/components/NavBar.js
+++ all_american_painting/src/components/NavBar.js
@@ -7,11 +7,13 @@ const NavBar = () => {
     return (
         <div>
             <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/Commercial">Commercial</Nav.Link>
+                    <Nav.Link href="/Residential">Residential</Nav.Link>
+                    <Nav.Link href="/Process">Process</Nav.Link>
+                    <Nav.Link href="/Contact">Contact</Nav.Link>
+
                 </Nav>
             </Navbar>
             <br />
