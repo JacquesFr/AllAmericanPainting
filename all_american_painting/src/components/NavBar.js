062bf8c0b4071d6885876e516a3a52fec6b5b6e4
Ashley Taylor McDaniel <43732431+ASHTM15@users.noreply.github.com>
2020-12-07 13:49:22 -0800

    Navbar works
    Navbar works but it refreshes the page each time a new page is clicked on.

--- all_american_painting/src/components/NavBar.js
+++ all_american_painting/src/components/NavBar.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Link from './Link';
 
 
 const NavBar = () => {
     return (
-        <div>
-            <Navbar bg="light" variant="light">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/Commercial">Commercial</Nav.Link>
-                    <Nav.Link href="/Residential">Residential</Nav.Link>
-                    <Nav.Link href="/Process">Process</Nav.Link>
-                    <Nav.Link href="/Contact">Contact</Nav.Link>
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                <a href="/" className="nav-link">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Commercial" className="nav-link">Commercial Painting</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Residential" className="nav-link">Residential Painting</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Process" className="nav-link">Our Process</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/Contact" className="nav-link">Contact Us</a>
+                </li>
 
-                </Nav>
-            </Navbar>
-            <br />
-        </div>
+            </ul>
+            
+            </div>
+            </nav>
+          
         
     );
 };
--- all_american_painting/src/components/Route.js
+++ all_american_painting/src/components/Route.js
@@ -1,7 +1,20 @@
+import { useEffect, useState} from 'react';
 
 const Route = ({path, children}) => {
-    return window.location.pathname === path
-    ? children: null;
+    useEffect(() => {
+        const onLocationChange = () =>{
+            console.log('Location Chnage');
+        };
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
+    },[]);
+    return window.location.pathname === path ? children: null;
 };
+
+
 
 export default Route;
