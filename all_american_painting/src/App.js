29de3bf8d1c6b88f52913523024f9c4712178fb0
Ashley Taylor McDaniel <43732431+ASHTM15@users.noreply.github.com>
2020-12-08 17:57:12 -0800

    working navbar without NavLink

--- all_american_painting/src/App.js
+++ all_american_painting/src/App.js
@@ -1,16 +1,75 @@
-import React from 'react';
-
-import Header from './components/Header';
-import Home from './components/Home';
-import Route from './components/Route';
-import Commercial from './components/Commercial';
-import Residential from './components/Residential';
-import Contact from './components/Contact';
-import Process from './components/Process';
-import NavBar from './components/NavBar';
+import React, { Component } from 'react';
+import './myStyle.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Home, Commercial, Residential, Process, Contact } from './pages' ;
 
 
-class App extends React.Component {
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path = '/' component={Home} />
+            <Route exact path = '/Commercial' component={Commercial} />
+            <Route exact path = '/Residential' component={Residential} />
+            <Route exact path = '/Process' component={Process} />
+            <Route exact path = '/Contact' component={Contact} />
+
+        </Switch>
+    </main>
+)
+
+class NavLink extends Component {
+    render() {
+      return (
+        <li className={"nav-item " + (this.props.isActive ? "active": "")}>
+                  <Link className="nav-link" to={this.props.path}>{this.props.text}</Link>
+        </li>
+      );
+  }
+}
+
+const Header1 = () => (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-light justify-content-center" >
+        <div class="mx-auto">   
+            <ul class="nav justify-content-center">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+
+                        <ul class="navbar-nav justify-content-center">
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/" >Home</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/Commercial" >Commercial Painting</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/Residential" >Residential Painting</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/Process" >Our Process</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/Contact" >Contact Us</Link>
+                                </li>
+                            </ul>
+                    </div>
+            </ul> 
+        </div>
+    </nav>
+  </div>
+)
+
+const App = () => (
+    <div>
+        <Header1 />
+        <Main />
+    </div>
+)
+
+export default App;
+/*class App extends React.Component {
     render() { 
         return (
             <div>
@@ -38,5 +97,4 @@ class App extends React.Component {
         );
     }
 };
-
-export default App;
+*/
---
+++ all_american_painting/src/pages.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export const Home = () => (
+    <div>
+        <h1>Home</h1>
+    </div>
+)
+
+export const Commercial = () => (
+    <div>
+        <h1>Commercial</h1>
+    </div>
+)
+
+export const Residential = () => (
+    <div>
+        <h1>Residential</h1>
+    </div>
+)
+
+export const Process = () => (
+    <div>
+        <h1>Process</h1>
+    </div>
+)
+
+export const Contact = () => (
+    <div>
+        <h1>Contact Us</h1>
+    </div>
+)
